53c900370889d2a464817681c93aa3c06f1d8de6
thomasjshea <thomasjshea89@gmail.com>
2023-07-06 18:40:31 -0500

    created queries and mutations front end file, wrote mutations code

---
+++ client/src/utils/mutations.js
@@ -0,0 +1,54 @@
+import { gql } from '@apollo/client'
+
+export const ADD_USER = gql`
+mutation addUser($username: String!, $email: String!, $password: String!, $address: String!) {
+    addUser(username: $username, email: $email, password: $password, address: $address) {
+        _id
+        username
+        email
+        address
+        products {
+            _id
+            name
+        }
+    }
+}
+`;
+
+export const LOGIN_USER = gql`
+mutation login($email: String!, $password: String!) {
+    login(email: $email, password: $password) {
+        _id
+        username
+        email
+        address
+        products {
+            _id
+            name
+        }
+    }
+}
+`;
+
+export const ADD_PRODUCT = gql`
+mutation addProduct($name: String!, $description: String!, $price: Float!, $image: String!, $link: String!) {
+    addProduct(name: $name, description: $description, price: $price, image: $image, link: $link) {
+        _id
+        name
+        description
+        price
+        image
+        link
+        addedBy
+    }
+}
+`;
+
+export const REMOVE_PRODUCT = gql`
+mutation removeProduct($productId: String!) {
+    removeProduct(productId: $productId) {
+        _id
+        name
+    }
+}
+`
