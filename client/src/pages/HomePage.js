2051203a48b51ed31526b1e7d807387008fdc95f
thomasjshea <thomasjshea89@gmail.com>
2023-07-12 19:05:42 -0500

    corrected errors

--- client/src/pages/HomePage.js
+++ client/src/pages/HomePage.js
@@ -1,5 +1,4 @@
 import React from "react";
-// import ProductItem from "../Components/ProductItem";
 import ProductList from "../Components/ProductList"
 // import Cart from "../Components/Cart";
 import { useQuery } from '@apollo/client'
